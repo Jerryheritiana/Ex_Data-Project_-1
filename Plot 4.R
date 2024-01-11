3415e71b9eec95a8cde08e6fab838bf68546a49c
Jerry Heritiana Hajason <152583451+Jerryheritiana@users.noreply.github.com>
2024-01-11 11:37:27 -0500

    fourth plot

---
+++ Plot 4.R
@@ -0,0 +1,70 @@
+library(tibble)
+library(dplyr)
+library(lubridate)
+
+## download data ##
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file <- "electricpowerconsumption.zip"
+download.file(url, file, method= "curl")
+
+## Uzip File
+unzip("electricpowerconsumption.zip")
+
+## Load files
+hpc <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+## Conversion en tibble
+hpc <- as.tibble(hpc)
+
+## Subseting data
+data <- rbind(subset(hpc, Date == "1/2/2007"), subset(hpc, Date == "2/2/2007"))
+head(data)
+rm(hpc)
+
+# Itérer manuellement sur chaque colonne avec une boucle pour numériser
+for (colonne in names(data[-c(1,2)])) {
+        data <- data %>%
+                mutate(!!colonne := as.numeric(.data[[colonne]])) %>% ## !!colonne pour s'assurer que le nom de la colonne 
+                ##  est correctement évalué dans le contexte de la boucle
+                suppressWarnings()
+}   
+
+## Adding a new col with Ftime and mutate it to date class
+data$Ftime<- paste(data$Date, data$Time, sep = " ")
+data <- data %>%
+        mutate(Ftime = strptime(Ftime, format = "%d/%m/%Y %H:%M:%S"))
+
+## Select/arrange column that we are interest
+data <- data %>% 
+        select(Date, Time, Ftime, Global_active_power, Sub_metering_1,Sub_metering_2, Sub_metering_3, Voltage, Global_reactive_power )
+
+## Ploting for plot in one files
+
+png(filename = "Plot 4.png")
+par(mfrow = c(2, 2))
+
+## plot Time with global active power
+with(data, plot(Ftime, Global_active_power, type ="l",xaxt = "n" ,xlab = " ", ylab="Global Active Power "))
+## x- axis units
+axis_unit <- strptime(c("2007-02-01 00:00:00", "2007-02-02 00:00:00", "2007-02-03 00:00:00"),format = "%Y-%m-%d %H:%M:%S")
+axis_unit <- as.POSIXct(axis_unit)
+axis(1, at = axis_unit , labels = format(axis_unit, format = "%a"))
+
+### Plot Voltage
+with(data, plot(Ftime, Voltage, type = "l", xlab = "datetime", ylab = "Voltage", xaxt ="n"))
+axis(1, at = axis_unit , labels = format(axis_unit, format = "%a"))
+
+### Plot Energy sub metering
+with(data, plot(Ftime, Sub_metering_1, col = "black", type ="l", xaxt = "n", ylab = "Energy Sub Metering", xlab = " ")) ## Sub_metering_1 
+lines(data$Ftime, data$Sub_metering_2, col = "red", type = "l", xaxt = "n") ## Sub_metering_2
+lines(data$Ftime, data$Sub_metering_3, col ="blue", type ="l", xaxt = "n") ## Sub_metering_3
+axis(1, at = axis_unit , labels = format(axis_unit, format = "%a"))
+#### legend
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, bty = "n")
+
+### Plot Global_relative_power
+with(data, plot (Ftime, Global_reactive_power, xaxt = "n", xlab = "datetime", ylab = "Global_reactive_power", type = "l"))
+axis_unit <- as.POSIXct(axis_unit)
+axis(1, at = axis_unit , labels = format(axis_unit, format = "%a"))
+
+dev.off()
